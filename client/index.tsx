892ee5f849f3b2cbf6ba71c2a1e5d792afec5a7c
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2021-02-12 17:14:59 -0500

    .

--- client/index.tsx
+++ client/index.tsx
@@ -1,12 +1,6 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-
-class App extends React.Component {
-    render() {
-        return <h1>Hi there</h1>;
-    }
-}
-
-const rootContainer = document.querySelector('#main');
-ReactDOM.render(<App />, rootContainer);
+const rootContainer = document.querySelector('#main')
+ReactDOM.render(<App />, rootContainer)
